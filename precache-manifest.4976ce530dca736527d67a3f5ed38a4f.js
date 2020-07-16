f6ada5c03d68bd6cbaa01f853a708a0acab88df2
siratul <asatulsingh20@gmail.com>
2020-07-16 22:58:25 +0530

    Updates

---
+++ precache-manifest.4976ce530dca736527d67a3f5ed38a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235768ebbadb6c38501233df52337bb2",
+    "url": "/Snake/index.html"
+  },
+  {
+    "revision": "0f47b7f2281135e940cb",
+    "url": "/Snake/static/css/main.d315d106.chunk.css"
+  },
+  {
+    "revision": "b591f57023943fb7923a",
+    "url": "/Snake/static/js/2.6f9a47dd.chunk.js"
+  },
+  {
+    "revision": "9599c64a244e954218e1e62d01fa014e",
+    "url": "/Snake/static/js/2.6f9a47dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f47b7f2281135e940cb",
+    "url": "/Snake/static/js/main.49d42eb5.chunk.js"
+  },
+  {
+    "revision": "7f650829b46b4335ad15",
+    "url": "/Snake/static/js/runtime-main.01a72eab.js"
+  }
+]);
